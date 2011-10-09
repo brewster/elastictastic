5be211b3f5db9a556325b4958a9e5636e3e43e07
Mat Brown <mat@cliqueinnyc.com>
2011-10-09 13:34:26 -0700

    In Rails, set default index from app name/environment

--- lib/elastictastic/railtie.rb
+++ lib/elastictastic/railtie.rb
@@ -2,11 +2,15 @@ module Elastictastic
   class Railtie < Rails::Railtie
     initializer "elastictastic.configure_rails" do
       config_path = Rails.root.join('config/elastictastic.yml').to_s
+      config = Elastictastic.config
+      app_name = Rails.application.class.name.split('::').first.underscore
+      config.default_index = "#{app_name}_#{Rails.env}"
+
       if File.exist?(config_path)
         yaml = YAML.load_file(config_path)[Rails.env]
         if yaml
           yaml.each_pair do |name, value|
-            Elastictastic.config.__send__("#{name}=", value)
+            config.__send__("#{name}=", value)
           end
         end
       end
