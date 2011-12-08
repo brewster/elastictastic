d895ef3c53e9035cea53f6825c3209132c3072c4
Aubrey Holland <aubrey@brewster.com>
2011-12-08 17:11:22 -0500

    handle nested exceptions and bump to 0.6.4

--- lib/elastictastic/server_error.rb
+++ lib/elastictastic/server_error.rb
@@ -1,6 +1,7 @@
 module Elastictastic
   module ServerError
     ERROR_PATTERN = /^([A-Z][A-Za-z]*)(?::\s*)?(.*)$/
+    NESTED_PATTERN = /^.*nested:\s+(.*)$/
 
     class ServerError < StandardError
       attr_accessor :status
@@ -16,10 +17,14 @@ module Elastictastic
       def [](server_message, status = nil)
         match = ERROR_PATTERN.match(server_message)
         if match
-          clazz = Elastictastic::ServerError.const_get(match[1])
-          error = clazz.new(match[2])
-          error.status = status
-          error
+          if (nested_match = NESTED_PATTERN.match(match[2]))
+            return self[nested_match[1], status]
+          else
+            clazz = Elastictastic::ServerError.const_get(match[1])
+            error = clazz.new(match[2])
+            error.status = status
+            error
+          end
         else
           Elastictastic::ServerError::ServerError.new(server_message)
         end
