eeb5b8719eb5cd2b9ddd29368d7537cb91f6fbfd
Mat Brown <mat.a.brown@gmail.com>
2012-03-27 12:59:11 -0400

    Only autoload non-core parts of the library

--- lib/elastictastic/resource.rb
+++
@@ -1,4 +0,0 @@
-warn 'Elastictastic::Resource is deprecated and will be removed in a future version. The new module name is Elastictastic::EmbeddedDocument'
-module Elastictastic
-  Resource = EmbeddedDocument
-end
--- lib/elastictastic/scope.rb
+++ lib/elastictastic/scope.rb
@@ -1,4 +1,5 @@
 require 'hashie'
+require 'elastictastic/search'
 
 module Elastictastic
   class Scope < BasicObject
