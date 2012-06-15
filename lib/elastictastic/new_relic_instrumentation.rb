7a53b697594e8709a4a89ef676cf3e260bef64c0
Mat Brown <mat.a.brown@gmail.com>
2012-06-15 11:50:28 -0400

    Change format of NewRelic categories

--- lib/elastictastic/new_relic_instrumentation.rb
+++ lib/elastictastic/new_relic_instrumentation.rb
@@ -11,14 +11,14 @@ module Elastictastic
     included do
       include NewRelic::Agent::MethodTracer
 
-      add_method_tracer :create, 'ElasticSearch/#{args[1].classify}#create'
-      add_method_tracer :delete, 'ElasticSearch/#{args[1].classify + "#" if args[1]}/delete'
-      add_method_tracer :get, 'ElasticSearch/#{args[1].classify}#get'
-      add_method_tracer :mget, 'ElasticSearch/mget'
-      add_method_tracer :put_mapping, 'ElasticSearch/#{args[1].classify}#put_mapping'
-      add_method_tracer :scroll, 'ElasticSearch/scroll'
-      add_method_tracer :search, 'ElasticSearch/#{args[1].classify}#search'
-      add_method_tracer :update, 'ElasticSearch/#{args[1].classify}#update'
+      add_method_tracer :create, 'Database/ElasticSearch/create'
+      add_method_tracer :delete, 'Database/ElasticSearch/delete'
+      add_method_tracer :get, 'Database/ElasticSearch/get'
+      add_method_tracer :mget, 'Database/ElasticSearch/mget'
+      add_method_tracer :put_mapping, 'Database/ElasticSearch/put_mapping'
+      add_method_tracer :scroll, 'Database/ElasticSearch/scroll'
+      add_method_tracer :search, 'Database/ElasticSearch/search'
+      add_method_tracer :update, 'Database/ElasticSearch/update'
     end
   end
 end
